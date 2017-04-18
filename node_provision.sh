c879536a0aaa332fa89540750fabca836a8aaf63
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-18 21:05:30 +0300

    t7

--- node_provision.sh
+++ node_provision.sh
@@ -8,5 +8,7 @@ grep -q -F '192.168.10.102 puppet-node1' /etc/hosts || echo '192.168.100.102 pup
 
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 
+puppet agent -t --verbose --debug
+
 echo '0'
 exit 0
--- puppet/site.pp
+++ puppet/site.pp
@@ -1,4 +1,4 @@
-node 'node' {
+node 'puppet-node1.epbyminw2473.minsk.epam.com' {
    include jboss
    include deploy
  } 
