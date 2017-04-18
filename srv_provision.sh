ad0567cb7b90992eff5cbffa4dd051c91d4c7d34
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-18 22:21:29 +0300

    fixes

--- srv_provision.sh
+++ srv_provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-grep -q -F '192.168.10.101 puppet-server' /etc/hosts || echo '192.168.100.101 puppet puppet-server puppet-server.epbyminw2473.minsk.epam.com' >> /etc/hosts
-grep -q -F '192.168.10.102 puppet-node1' /etc/hosts || echo '192.168.100.102 puppet-node1 puppet-node1.epbyminw2473.minsk.epam.com' >> /etc/hosts
+grep -q -F '192.168.10.101 puppet-server' /etc/hosts || echo '192.168.10.101 puppet puppet-server puppet-server.epbyminw2473.minsk.epam.com' >> /etc/hosts
+grep -q -F '192.168.10.102 puppet-node1' /etc/hosts || echo '192.168.10.102 puppet-node1 puppet-node1.epbyminw2473.minsk.epam.com' >> /etc/hosts
 
 yum install -y epel-release > /dev/null 2>&1
 yum localinstall -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm > /dev/null 2>&1
