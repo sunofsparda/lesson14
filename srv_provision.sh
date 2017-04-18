55380f593fedc1950da1d603eb3f61aea4e28aef
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-18 19:44:49 +0300

    Update srv_provision.sh

--- srv_provision.sh
+++ srv_provision.sh
@@ -4,6 +4,7 @@ systemctl restart network
 cp /vagrant/puppet/autosign.conf /etc/puppetlabs/puppet   
 systemctl start puppetserver
 source ~/.bashrc
+mkdir -p /etc/puppetlabs/code/environments/production/modules
 cp -R /vagrant/puppet/modules/* /etc/puppetlabs/code/environments/production/modules
 chmod -R 0777 /etc/puppetlabs/code/environments/production/modules/jboss/files
 cp /vagrant/puppet/site.pp /etc/puppetlabs/code/environments/production/manifests
