471c5ea3b889fcf3d6387ff9b9aa3776d027bf0e
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-18 21:08:42 +0300

    t8

--- node_provision.sh
+++ node_provision.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y puppet-agent
-systemctl restart network
+
 echo "server = puppet-server.epbyminw2473.minsk.epam.com" >> /etc/puppetlabs/puppet/puppet.conf
 grep -q -F '192.168.10.101 puppet-server' /etc/hosts || echo '192.168.100.101 puppet-server.epbyminw2473.minsk.epam.com' >> /etc/hosts
 grep -q -F '192.168.10.102 puppet-node1' /etc/hosts || echo '192.168.100.102 puppet-node1.epbyminw2473.minsk.epam.com' >> /etc/hosts
