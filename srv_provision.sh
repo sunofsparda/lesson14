9cac343af9e225c1fba168b3755b8de30f45b4aa
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-18 19:54:10 +0300

    t1

--- srv_provision.sh
+++ srv_provision.sh
@@ -1,10 +1,22 @@
-rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum install -y puppetserver
-systemctl restart network
+#!/bin/bash
+
+
+grep -q -F '192.168.10.101 puppet-server' /etc/hosts || echo '192.168.100.101 puppet-server.epbyminw2473.minsk.epam.com' >> /etc/hosts
+grep -q -F '192.168.10.102 puppet-node1' /etc/hosts || echo '192.168.100.102 puppet-node1.epbyminw2473.minsk.epam.com' >> /etc/hosts
+
+
+yum install -y epel-release > /dev/null 2>&1
+yum localinstall -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm > /dev/null 2>&1
+yum install -y puppetserver > /dev/null 2>&1
+
+
 cp /vagrant/puppet/autosign.conf /etc/puppetlabs/puppet   
-systemctl start puppetserver
 source ~/.bashrc
 mkdir -p /etc/puppetlabs/code/environments/production/modules
 cp -R /vagrant/puppet/modules/* /etc/puppetlabs/code/environments/production/modules
 chmod -R 0777 /etc/puppetlabs/code/environments/production/modules/jboss/files
 cp /vagrant/puppet/site.pp /etc/puppetlabs/code/environments/production/manifests
+
+
+systemctl enable puppetserver
+systemctl restart puppetserver
